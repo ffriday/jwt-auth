9e01b3aac5fc742208d182b7cc545cd0a0ed581a
mitrich <4irtim@gmail.com>
2024-01-08 19:46:48 +0300

    Front project setup (#1)
    * feat: add vite-react
    
    * feat: add eslint and airbnb style
    
    * feat: setup airbnb
    
    * feat: add prettier
    
    * feat: add test config
    
    * feat: add husky
    
    * feat: add husky config
    
    * fix: husky config
    
    * fix: husky config
    
    * fix: ad husky global
    
    * fix: ad husky global
    
    * fix: ad husky global
    
    * feat: add PR template

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "airbnb",
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:react/recommended",
+    "standard-with-typescript",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json", "./tsconfig.node.json", "testConfig.ts"],
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "react/react-in-jsx-scope": 0,
+    "@typescript-eslint/triple-slash-reference": 0,
+    "@typescript-eslint/prefer-nullish-coalescing": 0,
+    "@typescript-eslint/strict-boolean-expressions": 0,
+  },
+};
