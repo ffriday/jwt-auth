d89fe604d3061d7c89464f2832f63dec6b096c7c
mitrich <4irtim@gmail.com>
2024-01-10 13:09:50 +0300

    feat: add header footer

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,5 @@
+import styles from './Footer.module.scss';
+
+export default function Footer(): JSX.Element {
+  return <footer className={styles.footer}>JWT-AUTH</footer>;
+}
--- frontend/src/components/News/News.tsx
+++ frontend/src/components/News/News.tsx
@@ -1,13 +1,27 @@
+/* eslint-disable prettier/prettier */
 import { Grid } from '@mui/material';
 
 export default function News(): JSX.Element {
   return (
     <>
       <Grid item xs={12} sm={6}>
-        LEFT
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est deleniti
+        cupiditate ea aperiam iure incidunt dolore neque reprehenderit nobis
+        laboriosam, soluta nostrum officiis pariatur suscipit voluptates dolor
+        placeat dolorem labore? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Est deleniti cupiditate ea aperiam iure incidunt
+        dolore neque reprehenderit nobis laboriosam, soluta nostrum officiis
+        pariatur suscipit voluptates dolor placeat dolorem labore?
       </Grid>
       <Grid item xs={12} sm={6}>
-        RIGHT
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+        voluptatum reiciendis eos at, repellendus laboriosam reprehenderit
+        blanditiis molestias excepturi placeat. Facere quos corporis tempore
+        dignissimos iste. Vitae similique aperiam sint. Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Est deleniti cupiditate ea aperiam
+        iure incidunt dolore neque reprehenderit nobis laboriosam, soluta
+        nostrum officiis pariatur suscipit voluptates dolor placeat dolorem
+        labore?
       </Grid>
     </>
   );
