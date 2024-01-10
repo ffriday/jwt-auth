d30c7e39a7c9ebff29ead72d696fb4ce20b7e561
mitrich <4irtim@gmail.com>
2024-01-10 11:16:25 +0300

    feat: add header logout button

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,25 +1,45 @@
 import { Button } from '@mui/material';
 import LoginIcon from '@mui/icons-material/Login';
 import HowToRegIcon from '@mui/icons-material/HowToReg';
+import LogoutIcon from '@mui/icons-material/Logout';
 import { useResize } from '../../hooks';
-
-import styles from './Header.module.scss';
 import { MEDIA_MOBILE } from '../../constants/constants';
 
-export default function Header(): JSX.Element {
-  const { width } = useResize();
+import styles from './Header.module.scss';
 
+enum HeaderText {
+  caption = 'JWT-AUTH',
+  login = 'Log In',
+  signup = 'Sign Up',
+  logout = 'Log Out',
+}
+
+const isLogged = false; // TODO - change to Auth hook
+
+export default function Header(): JSX.Element {
   return (
     <header className={styles.header}>
-      <h1>JWT-AUTH</h1>
-      <div className={styles.buttons}>
-        <Button variant="outlined" startIcon={<LoginIcon />}>
-          {width > MEDIA_MOBILE ? 'Log In' : ''}
-        </Button>
-        <Button variant="outlined" startIcon={<HowToRegIcon />}>
-          {width > MEDIA_MOBILE ? 'Sign Up' : ''}
-        </Button>
-      </div>
+      <h1>{HeaderText.caption}</h1>
+      <AuthButtons />
     </header>
+  );
+}
+
+function AuthButtons(): JSX.Element {
+  const { width } = useResize();
+
+  return !isLogged ? (
+    <div className={styles.buttons}>
+      <Button variant="outlined" startIcon={<LoginIcon />}>
+        {width > MEDIA_MOBILE ? HeaderText.login : ''}
+      </Button>
+      <Button variant="outlined" startIcon={<HowToRegIcon />}>
+        {width > MEDIA_MOBILE ? HeaderText.signup : ''}
+      </Button>
+    </div>
+  ) : (
+    <Button variant="outlined" startIcon={<LogoutIcon />}>
+      {width > MEDIA_MOBILE ? HeaderText.logout : ''}
+    </Button>
   );
 }
--- frontend/src/hooks/useResize.tsx
+++ frontend/src/hooks/useResize.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useLayoutEffect, useState } from 'react';
 
 type Resize = {
   width: number;
@@ -8,7 +8,7 @@ type Resize = {
 export default function useResize(): Resize {
   const [[width, height], setSize] = useState([0, 0]);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     const resize = (): void => setSize([window.innerWidth, window.innerHeight]);
     window.addEventListener('resize', resize);
     return (): void => window.removeEventListener('resize', resize);
