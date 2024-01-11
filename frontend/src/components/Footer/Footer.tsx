946e37266c46f2581a837cb1b17a1c49229c79a9
mitrich <4irtim@gmail.com>
2024-01-11 20:59:21 +0300

    feat: add api status message (#3)

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,5 +1,40 @@
+import { useEffect, useState } from 'react';
+import SignalWifiStatusbar4BarIcon from '@mui/icons-material/SignalWifiStatusbar4Bar';
+import SignalWifiStatusbarConnectedNoInternet4Icon from '@mui/icons-material/SignalWifiStatusbarConnectedNoInternet4';
+
+import axios from 'axios';
 import styles from './Footer.module.scss';
+import { Routes, STATUS_OK, URL } from '../../constants/constants';
+
+type statusResponse = {
+  name: string;
+  message: string;
+};
 
 export default function Footer(): JSX.Element {
-  return <footer className={styles.footer}>JWT-AUTH</footer>;
+  return (
+    <footer className={styles.footer}>
+      <p>JWT-AUTH</p>
+      <Status />
+    </footer>
+  );
+}
+
+function Status(): JSX.Element {
+  const [status, setStatus] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get<statusResponse>(`http://${URL()}/${Routes.Status}`)
+      .then((res) => {
+        setStatus(res.data.message === STATUS_OK);
+      })
+      .catch(() => setStatus(false));
+  }, []);
+
+  return status ? (
+    <SignalWifiStatusbar4BarIcon color="success" />
+  ) : (
+    <SignalWifiStatusbarConnectedNoInternet4Icon />
+  );
 }
--- frontend/src/constants/constants.ts
+++ frontend/src/constants/constants.ts
@@ -1,1 +1,12 @@
 export const MEDIA_MOBILE = 430;
+
+export const HOST = 'localhost';
+export const PORT = '8080';
+export const URL = (): string => `${HOST}:${PORT}`;
+export const STATUS_OK = 'OK';
+
+export enum Routes {
+  Root = '/',
+  Status = 'status',
+  Auth = 'auth',
+}
